da2bab9833e3acc15c482b9228b87e1665bd4fed
AntonSkyrda <mr.skyrda@gmail.com>
2025-03-30 23:48:48 +0300

    create accses token model

--- core/models/user.py
+++ core/models/user.py
@@ -1,21 +1,22 @@
 from typing import TYPE_CHECKING
 
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
+from core.types.user_id import UserIdType
 from .base import BaseModel
 from .mixins.id_int_pk import IdIntPkMixin
-
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
 
 
-class User(BaseModel, IdIntPkMixin, SQLAlchemyBaseUserTable[int]):
-    first_name: Mapped[str] = mapped_column()
-    last_name: Mapped[str] = mapped_column()
-    father_name: Mapped[str] = mapped_column()
+class User(BaseModel, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
+    first_name: Mapped[str] = mapped_column(String)
+    last_name: Mapped[str] = mapped_column(String)
+    father_name: Mapped[str] = mapped_column(String)
 
     @classmethod
     def get_db(cls, session: AsyncSession) -> SQLAlchemyUserDatabase:
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
