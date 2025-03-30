4cac9edefedd7008263fa0d09caa0d3bd4fc2ff9
AntonSkyrda <mr.skyrda@gmail.com>
2025-03-31 01:42:28 +0300

    update config.py

--- core/config.py
+++ core/config.py
@@ -1,7 +1,6 @@
 from pathlib import Path
-
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 BASE_DIR = Path(__file__).parent.parent
@@ -9,12 +8,22 @@ BASE_DIR = Path(__file__).parent.parent
 
 class AccessToken(BaseModel):
     lifetime_seconds: int = 3600
+    reset_password_token_secret: str
+    verification_token_secret: str
 
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = f"sqlite+aiosqlite:///./{BASE_DIR}/db.sqlite3"
+    db_url: str = f"sqlite+aiosqlite:///{BASE_DIR}/db.sqlite3"
     db_echo: bool = True
-    access_token: AccessToken = AccessToken()
+    access_token: AccessToken
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG"
+    )
+
 
 settings = Settings()
