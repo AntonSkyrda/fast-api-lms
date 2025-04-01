59cd7ac17095ec7ee4bab54e9a64302380f3f015
AntonSkyrda <mr.skyrda@gmail.com>
2025-04-01 13:09:15 +0300

    add cors middleware to connect client to server

--- main.py
+++ main.py
@@ -2,18 +2,39 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import AsyncGenerator
 
 from api_v1 import router as router_v1
 from core.config import settings
+from core.models import db_helper
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
-
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
+    # startup
     yield
+    # shutdown
+    await db_helper.dispose()
 
 
 app = FastAPI(lifespan=lifespan)
+
+
+origins = [
+    "http://localhost:3000",
+    "https://127.0.0.1:3000",
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
