d659ae0d5d6c0017c3cbacb671fed7b8eb9e1c21
AntonSkyrda <mr.skyrda@gmail.com>
2025-04-08 00:21:09 +0300

    create course and group models and create association relations

---
+++ core/models/course.py
@@ -0,0 +1,20 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .mixins.id_int_pk import IdIntPkMixin
+from .base import BaseModel
+from .course_group_association import course_group_association_table
+
+if TYPE_CHECKING:
+    from .group import Group
+
+
+class Course(IdIntPkMixin, BaseModel):
+    name: Mapped[str] = mapped_column(String(25))
+    description: Mapped[str] = mapped_column(String(255))
+
+    groups: Mapped[list["Group"]] = relationship(
+        secondary=course_group_association_table, back_populates="courses"
+    )
---
+++ core/models/group.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .mixins.id_int_pk import IdIntPkMixin
+from .base import BaseModel
+from .course_group_association import course_group_association_table
+
+if TYPE_CHECKING:
+    from .course import Course
+
+
+class Group(IdIntPkMixin, BaseModel):
+    name: Mapped[str] = mapped_column(String(25))
+    year_of_study: Mapped[int] = mapped_column(Integer)
+
+    courses: Mapped[list["Course"]] = relationship(
+        secondary=course_group_association_table,
+        back_populates="groups",
+    )
