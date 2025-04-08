1028d9299829b11da655c838656a93aa9165215f
AntonSkyrda <mr.skyrda@gmail.com>
2025-04-08 16:41:03 +0300

    create course and group crud

---
+++ core/crud/courses.py
@@ -0,0 +1,67 @@
+from sqlalchemy import select, update, delete
+from sqlalchemy.engine import Result
+from sqlalchemy.orm import selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import Course
+from core.schemas.courses import CourseCreate, CourseUpdate
+
+
+async def get_course(session: AsyncSession, course_id: int) -> Course | None:
+    result = await session.execute(
+        select(Course)
+        .options(selectinload(Course.groups))
+        .where(Course.id == course_id)
+    )
+    return result.scalars().first()
+
+
+async def get_courses(session: AsyncSession) -> list[Course]:
+    stmt = select(Course).order_by(Course.id).options(selectinload(Course.groups))
+    result: Result = await session.execute(stmt)
+    courses = result.scalars().all()
+    return list(courses)
+
+
+async def create_course(session: AsyncSession, course_in: CourseCreate) -> Course:
+    course = Course(**course_in.model_dump())
+    session.add(course)
+    await session.commit()
+    await session.refresh(course)
+
+    result = await session.execute(
+        select(Course)
+        .options(selectinload(Course.groups))
+        .where(Course.id == course.id)
+    )
+
+    return result.scalar_one()
+
+
+async def update_course(
+    session: AsyncSession, course_id: int, course_in: CourseUpdate
+) -> Course:
+    stmt = (
+        update(Course)
+        .where(Course.id == course_id)
+        .values(**course_in.dict(exclude_unset=True))
+        .execution_options(synchronize_session="fetch")
+    )
+    await session.execute(stmt)
+    await session.commit()
+
+    result = await session.execute(select(Course).where(Course.id == course_id))
+    return result.scalar_one()
+
+
+async def delete_course(session: AsyncSession, course_id: int) -> Course | None:
+    result = await session.execute(select(Course).where(Course.id == course_id))
+    course = result.scalar_one_or_none()
+
+    if course is None:
+        return None
+
+    stmt = delete(Course).where(Course.id == course_id)
+    await session.execute(stmt)
+    await session.commit()
+    return course
---
+++ core/crud/groups.py
@@ -0,0 +1,63 @@
+from sqlalchemy import select, update, delete
+from sqlalchemy.engine import Result
+from sqlalchemy.orm import selectinload
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models import Group
+from core.schemas.groups import GroupCreate, GroupUpdate
+
+
+async def get_group(session: AsyncSession, group_id: int) -> Group | None:
+    result = await session.execute(
+        select(Group).options(selectinload(Group.courses)).where(Group.id == group_id)
+    )
+    return result.scalars().first()
+
+
+async def get_groups(session: AsyncSession) -> list[Group]:
+    stmt = select(Group).order_by(Group.id).options(selectinload(Group.courses))
+    result: Result = await session.execute(stmt)
+    groups = result.scalars().all()
+    return list(groups)
+
+
+async def create_group(session: AsyncSession, group_in: GroupCreate) -> Group:
+    group = Group(**group_in.model_dump())
+    session.add(group)
+    await session.commit()
+    await session.refresh(group)
+
+    result = await session.execute(
+        select(Group).options(selectinload(Group.courses)).where(Group.id == group.id)
+    )
+
+    return result.scalar_one()
+
+
+async def update_group(
+    session: AsyncSession, group_id: int, group_in: GroupUpdate
+) -> Group:
+
+    stmt = (
+        update(Group)
+        .where(Group.id == group_id)
+        .values(**group_in.dict(exclude_unset=True))
+        .execution_options(synchronize_session="fetch")
+    )
+    await session.execute(stmt)
+    await session.commit()
+    result = await session.execute(select(Group).where(Group.id == group_id))
+    return result.scalar_one()
+
+
+async def delete_group(session: AsyncSession, group_id: int) -> Group | None:
+
+    result = await session.execute(select(Group).where(Group.id == group_id))
+    group = result.scalar_one_or_none()
+    if group is None:
+        return None
+
+    stmt = delete(Group).where(Group.id == group_id)
+    await session.execute(stmt)
+    await session.commit()
+    return group
