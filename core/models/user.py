b1a6d4be160ff78ae95a53cfd240a6bcb305cd00
AntonSkyrda <mr.skyrda@gmail.com>
2025-04-09 01:30:47 +0300

    add relation between group and student

--- core/models/user.py
+++ core/models/user.py
@@ -1,7 +1,7 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 
 from core.types.user_id import UserIdType
@@ -10,6 +10,7 @@ from .mixins.id_int_pk import IdIntPkMixin
 
 if TYPE_CHECKING:
     from sqlalchemy.ext.asyncio import AsyncSession
+    from .group import Group
 
 
 class User(BaseModel, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
@@ -18,6 +19,12 @@ class User(BaseModel, IdIntPkMixin, SQLAlchemyBaseUserTable[UserIdType]):
     father_name: Mapped[str] = mapped_column(String)
     is_teacher: Mapped[bool] = mapped_column(Boolean, default=False)
     is_student: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    group_id: Mapped[int | None] = mapped_column(
+        Integer, ForeignKey("group.id"), nullable=True
+    )
+
+    group: Mapped["Group"] = relationship("Group", back_populates="students")
 
     @classmethod
     def get_db(cls, session: "AsyncSession") -> SQLAlchemyUserDatabase:
