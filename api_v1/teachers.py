e85fdd49bb4ede5c0a865e259f5d6be154707542
AntonSkyrda <mr.skyrda@gmail.com>
2025-04-14 18:58:58 +0300

    refactor get teachers endpoint for work with pagionation

--- api_v1/teachers.py
+++ api_v1/teachers.py
@@ -1,22 +1,39 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select
+from sqlalchemy import select, func
 
 from core.models.user import User
 from core.models import db_helper
 from core.schemas.user import UserRead
-
+from core.schemas.pagination import PaginationResponse
 
 router = APIRouter(prefix="/teachers", tags=["Teachers"])
 
 
-@router.get("/", response_model=list[UserRead])
-async def get_teachers(session: AsyncSession = Depends(db_helper.session_getter)):
-    result = await session.execute(select(User).where(User.is_teacher.is_(True)))
+@router.get("/", response_model=PaginationResponse[UserRead])
+async def get_teachers(
+    limit: int = Query(10, ge=1),
+    offset: int = Query(0, ge=0),
+    session: AsyncSession = Depends(db_helper.session_getter),
+):
+    total = await session.scalar(
+        select(func.count()).select_from(User).where(User.is_teacher.is_(True))
+    )
+
+    result = await session.execute(
+        select(User)
+        .where(User.is_teacher.is_(True))
+        .order_by(User.id)
+        .offset(offset)
+        .limit(limit)
+    )
     teachers = result.scalars().all()
+
     if not teachers:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="No teachers found",
-        )
-    return teachers
+        if not teachers:
+            raise HTTPException(status_code=404, detail="No students found")
+
+    return PaginationResponse(
+        total=total or 0,
+        items=[UserRead.model_validate(t) for t in teachers],
+    )
