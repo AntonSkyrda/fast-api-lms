508788071fc98d2accb8702557fb73d3798c5c84
AntonSkyrda <mr.skyrda@gmail.com>
2025-04-14 18:27:24 +0300

    fix pagination schema

--- core/schemas/pagination.py
+++ core/schemas/pagination.py
@@ -1,12 +1,11 @@
 from typing import Generic, TypeVar
 
 from pydantic import BaseModel
-from pydantic.generics import GenericModel
 
 
 T = TypeVar("T")
 
 
 class PaginationResponse(BaseModel, Generic[T]):
-    total = int
+    total: int
     items: list[T]
