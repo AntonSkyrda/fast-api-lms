2a6121c337b736cf16f7e94a37c896474f9ccd34
AntonSkyrda <mr.skyrda@gmail.com>
2025-04-16 22:21:55 +0300

    add serach by name/surname for teachers

--- api_v1/teachers.py
+++ api_v1/teachers.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, func
+from sqlalchemy import select, func, or_
 
 from core.models.user import User
 from core.models import db_helper
@@ -12,26 +12,28 @@ router = APIRouter(prefix="/teachers", tags=["Teachers"])
 
 @router.get("/", response_model=PaginationResponse[UserRead])
 async def get_teachers(
+    search: str | None = Query(None, description="Search by name, surname"),
     limit: int = Query(10, ge=1),
     offset: int = Query(0, ge=0),
     session: AsyncSession = Depends(db_helper.session_getter),
 ):
-    total = await session.scalar(
-        select(func.count()).select_from(User).where(User.is_teacher.is_(True))
-    )
+    base_stmt = select(User).where(User.is_teacher.is_(True))
+
+    if search:
+        base_stmt = base_stmt.where(
+            or_(
+                User.first_name.ilike(f"%{search}%"),
+                User.last_name.ilike(f"%{search}%"),
+            )
+        )
+
+    total_stmt = select(func.count()).select_from(base_stmt.subquery())
+    total = await session.scalar(total_stmt)
 
     result = await session.execute(
-        select(User)
-        .where(User.is_teacher.is_(True))
-        .order_by(User.id)
-        .offset(offset)
-        .limit(limit)
+        base_stmt.order_by(User.id).offset(offset).limit(limit)
     )
     teachers = result.scalars().all()
-
-    if not teachers:
-        if not teachers:
-            raise HTTPException(status_code=404, detail="No students found")
 
     return PaginationResponse(
         total=total or 0,
